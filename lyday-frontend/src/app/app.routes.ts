b0176a44aaed1311fa18cf5db152cad31c6de07b
VoidSantana <jpnsantanna2@gmail.com>
2026-03-28 20:01:43 -0300

    delete: deleção para aprendizado

--- lyday-frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './modules/auth/pages/login/login';
-
-export const routes: Routes = [
-  { path: "", component: LoginComponent},
-];
